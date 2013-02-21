41bf13c66b6caf6fe819568a363cc408a49639cc
Craig Peden <craig.peden.mail@gmail.com>
2013-02-21 11:59:31 +0000

    Booking

---
+++ application/controllers/booking.php
@@ -0,0 +1,29 @@
+<?php 
+
+	class Booking extends CI_Controller 
+	{
+
+		function __construct() 
+		{
+			
+			parent::__construct();
+			/*if (!$this->User_model->logged_in()) 
+			{
+				$this->session->set_flashdata('current_url', uri_string());
+				redirect('Login');	
+			}*/
+		}
+
+		// Main Page
+		
+		function index()
+		{				
+			$this->load->view('assets/header');
+			$this->load->view('assets/booking');
+			$this->load->view('assets/footer');
+		}
+		
+	}
+	
+/* End of file Namepending.php */
+/* Location: ../application/controllers */
