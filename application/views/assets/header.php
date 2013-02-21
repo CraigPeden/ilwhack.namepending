9e8a752730be8eb3ce1b98def55303c4f2bc6305
Thoob <tmscott10@hotmail.co.uk>
2013-02-21 16:43:48 +0000

    Tidied up title bar

--- application/views/assets/header.php
+++ application/views/assets/header.php
@@ -42,9 +42,9 @@
              <!-- Logged in as <a href="#" class="navbar-link">Username</a> -->
             </p>
             <ul class="nav">
-              <li class="active"><a href="#">Home</a></li>
+              <!-- <li class="active"><a href="#">Home</a></li>
               <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
+              <li><a href="#contact">Contact</a></li> --> 
             </ul>
           </div><!--/.nav-collapse -->
         </div>
