5739d81448952bbfc77902ee63ab27fd3e7084bb
Cameron Gray <cameron@camerongray.me>
2013-02-21 11:06:53 +0000

    Fixed minor error

--- application/controllers/javascript.php
+++ application/controllers/javascript.php
@@ -2,8 +2,8 @@
 class Javascript extends CI_Controller {
 	function index()
 	{
-		// This file outputs some data from CI to the header
-		frontend('Content-type: text/javascript');
+		// This file outputs some data from CI to the frontend
+		header('Content-type: text/javascript');
 
 		echo "var ci_site_url = '" . site_url() . "';\n";
 		echo "var ci_base_url = '" . base_url() . "';\n";
